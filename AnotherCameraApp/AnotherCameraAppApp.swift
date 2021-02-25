203f2795de34250553e4afce169cb1870d5f5072
Mkrefting <maxkrefting@yahoo.co.uk>
2021-02-25 12:42:30 +0000

    Initial Commit

---
+++ AnotherCameraApp/AnotherCameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnotherCameraAppApp.swift
+//  AnotherCameraApp
+//
+//  Created by Max Krefting on 25/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnotherCameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
